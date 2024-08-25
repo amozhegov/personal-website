50750333c9cc8706b67855727d4fac5d5188529b
amozhegov <kaneImfb@gmail.com>
2024-08-25 22:45:45 +0200

    Added Video background

--- src/components/Work_experience.js
+++ src/components/Work_experience.js
@@ -4,7 +4,6 @@ const Work_experience = () => {
   return (
     <section id="contact">
       <h2>Work Experience</h2>
-      <p>Email me at: <a href="mailto:your.email@example.com">your.email@example.com</a></p>
     </section>
   );
 };
