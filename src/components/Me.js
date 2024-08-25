2f24c2fd58f968cf699e291108a33f06172795c8
amozhegov <kaneImfb@gmail.com>
2024-08-25 03:19:15 +0200

    Added Vid as a background

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import profilePicture from '/Users/artem/Documents/Programming/JavaScript/personal-website/src/me.jpg';
+import profilePicture from '../me.jpg';
 import { FaEnvelope, FaInstagram, FaGithub, FaMapMarkerAlt } from 'react-icons/fa';
 import { IoLanguage } from 'react-icons/io5';
 import '../styles.css'; 
+import backgroundVideo from '../5818973-hd_1920_1080_24fps.mp4'; // Import the video
 
 const Me = () => {
   return (
     <section id="contact">
-      <div className='image-container'>
-          <img src={profilePicture} alt='Artem Mozhegov picture' className='profile-picture'/>
+      {/* Video Background */}
+      <video autoPlay muted loop className='background-video'>
+        <source src={backgroundVideo} type='video/mp4' />
+        Your browser does not support the video tag.
+      </video>
+
+      {/* Content Section */}
+      <div className='content'>
+        <div className='image-container'>
+            <img src={profilePicture} alt='Artem Mozhegov picture' className='profile-picture'/>
+        </div>
+        <h2>Artem Mozhegov</h2>
+        <p><FaMapMarkerAlt /> Spain</p>
+        <p><IoLanguage /> 🇬🇧 🇪🇸</p>
+        <p><FaEnvelope /> <a href='mailto:amozhegov@yahoo.com'>amozhegov@yahoo.com</a></p>
+        <p><FaInstagram /> <a href='https://www.instagram.com/artemmozhegov/'>artemmozhegov</a></p>
+        <p><FaGithub /> <a href='https://github.com/amozhegov'>artemmozhegov</a></p>
       </div>
-      <h2>Artem Mozhegov</h2>
-      <p> < FaMapMarkerAlt /> Spain</p>
-      <p>< IoLanguage /> 🇬🇧 🇪🇸</p>
-      <p>< FaEnvelope /> <a href='mailto:amozhegov@yahoo.com'>amozhegov@yahoo.com</a></p>
-      <p>< FaInstagram /> <a href='https://www.instagram.com/artemmozhegov/'>artemmozhegov</a></p>
-      <p>< FaGithub /> <a href='https://github.com/amozhegov'>artemmozhegov</a></p>
     </section>
   );
 };
